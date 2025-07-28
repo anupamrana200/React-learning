5f3d669d1e3b5c4f10970aa0243055246eafc74d
Anupam Rana <141600262+anupamrana200@users.noreply.github.com>
2025-07-28 23:14:27 +0530

    made some page components

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,23 @@ import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE"
+import Signup from "./Signup"
+import login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
 export {
   Header, 
   Footer,
   Container,
   Logo,
-  LogoutBtn
+  LogoutBtn,
+  RTE,
+  Signup,
+  login,
+  PostForm,
+  PostCard,
+  AuthLayout,
 }
